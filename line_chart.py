17c9471fe8cd94f7754f00581348560f12ffaf38
Arpit Agarwal <arpitagarwal1308@gmail.com>
2017-12-08 01:39:25 +0530

    Multi Lines

--- line_chart.py
+++ line_chart.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.arange(1,11)
-values = np.random.rand(10)
+y1 = x+5
+y2 = x**2
 
-plt.plot(x,values)
+plt.plot(x,y1)
+plt.plot(x,y2)
 
 plt.xlabel('ith Iteration')
 plt.ylabel('Random VariabLE')
